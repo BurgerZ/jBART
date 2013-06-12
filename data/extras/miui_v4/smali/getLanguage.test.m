615cfcd24b5189756ad8a6399c2e73396e2a2dc7
Andrei Zhukowski <az.burgerz@gmail.com>
2013-06-12 12:49:53 +0300

    update v0.7.8

---
+++ data/extras/miui_v4/smali/getLanguage.test.m
@@ -0,0 +1,108 @@
+.method public getLanguage()I
+    .locals 5
+
+    .prologue
+    .line 29
+    invoke-static {}, Ljava/util/Locale;->getDefault()Ljava/util/Locale;
+
+    move-result-object v4
+
+    invoke-virtual {v4}, Ljava/util/Locale;->getLanguage()Ljava/lang/String;
+
+    move-result-object v2
+
+    .line 30
+    .local v2, str1:Ljava/lang/String;
+    invoke-static {}, Ljava/util/Locale;->getDefault()Ljava/util/Locale;
+
+    move-result-object v4
+
+    invoke-virtual {v4}, Ljava/util/Locale;->getCountry()Ljava/lang/String;
+
+    move-result-object v3
+
+    .line 31
+    .local v3, str2:Ljava/lang/String;
+    const-string v4, "en"
+
+    invoke-virtual {v2, v4}, Ljava/lang/String;->equals(Ljava/lang/Object;)Z
+
+    move-result v4
+
+    if-nez v4, :cond_3
+
+    const-string v4, "ru"
+
+    invoke-virtual {v2, v4}, Ljava/lang/String;->equals(Ljava/lang/Object;)Z
+
+    move-result v4
+
+    if-nez v4, :cond_3
+
+    const/4 v0, 0x0
+
+    .line 32
+    .local v0, bool:Z
+    :goto_0
+    const/4 v1, 0x0
+
+    .line 33
+    .local v1, i:I
+    if-eqz v0, :cond_0
+
+    .line 34
+    const/4 v1, 0x1
+
+    .line 35
+    :cond_0
+    const-string v4, "zh"
+
+    invoke-virtual {v2, v4}, Ljava/lang/String;->equals(Ljava/lang/Object;)Z
+
+    move-result v4
+
+    if-eqz v4, :cond_2
+
+    invoke-static {v3}, Landroid/text/TextUtils;->isEmpty(Ljava/lang/CharSequence;)Z
+
+    move-result v4
+
+    if-nez v4, :cond_2
+
+    .line 37
+    const-string v4, "TW"
+
+    invoke-virtual {v3, v4}, Ljava/lang/String;->equals(Ljava/lang/Object;)Z
+
+    move-result v4
+
+    if-eqz v4, :cond_1
+
+    .line 38
+    const/4 v1, 0x2
+
+    .line 39
+    :cond_1
+    const-string v4, "CN"
+
+    invoke-virtual {v3, v4}, Ljava/lang/String;->equals(Ljava/lang/Object;)Z
+
+    move-result v4
+
+    if-eqz v4, :cond_2
+
+    .line 40
+    const/4 v1, 0x0
+
+    .line 42
+    :cond_2
+    return v1
+
+    .line 31
+    .end local v0           #bool:Z
+    .end local v1           #i:I
+    :cond_3
+    const/4 v0, 0x1
+
+    goto :goto_0
+.end method
---
+++ data/extras/miui_v4/smali/isCloudAntispamAvailable.m
@@ -0,0 +1,5 @@
+.method public static isCloudAntispamAvailable()Z
+    .locals 7
+    const/4 v0, 0x1
+    return v0
+.end method
